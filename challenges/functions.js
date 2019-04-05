8bfdd06d9ad2780aca8cfa7cfbfae498ea1cdb4c
van jordan <vanjordan46@hotmail.com>
2019-04-05 09:50:01 -0700

    Finished prototypes.js

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,18 +7,30 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
-
+function consume(num1,num2, cb){
+  return cb(num1,num2);
+}
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
-  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
+  * Create a function named greeting that accepts a first and last name
+  *  and returns "Hello first-name last-name, nice to meet you!"
 */
 
 
+function add(num1,num2){
+  return num1 + num2;
+}
+function multiply(num1,num2, ){
+  return num1 * num2;
+}
+function greeting(first, last){
+  return `Hello ${first} ${last}, nice to meet you!`;
+}
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
@@ -27,6 +39,8 @@
 
 // Explanation: 
 
+//The variable "internal" because it can reach outside it's scope to the outer scope. Variables 
+//inside can access variable outside but not vice-versa
 
 const external = "I'm outside the function";
 
